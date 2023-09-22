bd83b838a2c49e5d5db77c9e2938508bb1082412
Riremito <29014872+Riremito@users.noreply.github.com>
2023-09-23 01:11:38 +0900

    v417.2

--- TeresaBeta/ExportFunction.cpp
+++ TeresaBeta/ExportFunction.cpp
@@ -3,14 +3,8 @@
 #pragma intrinsic(_ReturnAddress)
 
 // Getting Return Address
-// 140D89F10 (v414.1) -> 140DDF350 (v415.1) -> 140DDF350 (v415.2)
-// 140DEE7B0 (v416.2)
 // 48 89 54 24 10 48 89 4C 24 08 56 57 48 83 EC 78 E9 1st result
-// @003C blocked = cannot be login
-// 14ACA130A
-// ULONG_PTR uReturnAddress = 0x14B0EC509; // v415.1
-// ULONG_PTR uReturnAddress = 0x14ACA130A; // v415.2
-ULONG_PTR uReturnAddress = 0x14AF8075D; // v416.2
+ULONG_PTR uReturnAddress = 0x14B4691F2; // v417.2 2023/09/23
 
 // NGClient64.aes+153A0
 #ifndef _WIN64
